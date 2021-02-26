f2526a26f7ad9edaf54977558416e323266a4ef8
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-26 18:58:35 -0300

    setting a menu style

---
+++ src/components/Menu/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+  background-color: #68DE5A;
+  height: 80px;
+  border-top-left-radius: 19;
+  border-top-right-radius: 19;
+  Text {
+    font-size: 10px;
+  }
+`;
+
+export const ItemLayout = styled.View`
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
