4c5e50819b25791bbdf6be7318ecbd2e693f3cbd
Igor Santos <igornzds@gmail.com>
2021-02-26 19:54:18 -0300

    update form login page

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, CardForm, TitleForm, InputForm, ButtonSubmit } from './styles';
 
 const Login: React.FC = () => {
   return (
     <Container>
-      <Text>Login</Text>
+      <CardForm>
+        <TitleForm>Seja bem vindo, informe seus dados para logar.</TitleForm>
+        <InputForm placeholder="Digite seu usuário" />
+        <InputForm placeholder="Digite seu senha" />
+
+        <ButtonSubmit>
+          <Text>OI</Text>
+        </ButtonSubmit>
+      </CardForm>
     </Container>
+
   );
 }
 
-export default Login;
+export default Login; 
--- src/screens/Login/styles.tsx
+++ src/screens/Login/styles.tsx
@@ -1,7 +1,38 @@
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
+    background-color: #8c52e5;
 `;
+
+export const CardForm = styled.View`
+  background: #fff;
+  width: 90%;
+  padding: 16px 32px;
+  border-radius: 8px;
+`;
+
+export const TitleForm = styled.Text`
+  font-size: 21px;
+  color: #1d1d1d;
+  margin-bottom: 60px;
+  text-align: center;
+`;
+
+export const InputForm = styled.TextInput`
+  width: 100%;
+  color: #878686;
+  border-color: #878686;
+  border-style: solid;
+  border-bottom-width: 1px;
+  font-size: 14px;
+  padding: 12px 6px;
+`
+export const ButtonSubmit = styled(RectButton)`
+  background: #68DE5A;
+  margin-top: 50px;
+  padding: 16px 32px;
+`
