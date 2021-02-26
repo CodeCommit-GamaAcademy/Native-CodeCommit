0cbc798c7154beac6f0ca4e7228f7c7e15a479cb
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-26 18:56:53 -0300

    creating a release's view

---
+++ src/screens/Dashboard/Releases/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Container } from './style';
+
+// import { Container } from './styles';
+
+const Releases: React.FC = () => {
+  return (
+    <Container>
+      <Text>
+        Lancamentos
+      </Text>
+    </Container>
+  );
+}
+
+export default Releases;
