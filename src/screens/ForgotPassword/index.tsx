4aaa9c99b890782820b6f0aa52b0bb6dc96340e8
Igor Santos <igornzds@gmail.com>
2021-02-27 18:00:03 -0300

    create validation function on ForgotPassword page

--- src/screens/ForgotPassword/index.tsx
+++ src/screens/ForgotPassword/index.tsx
@@ -1,45 +1,119 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Image } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
 import { Platform } from 'react-native';
 
-import { Container, ScrollContainer, AvoidContainer, LogoImage, Card, Title, InputContainer, Input, Button, ButtonText, ButtonLoginText, ButtonLogin  } from './styles';
+import { Container, ScrollContainer, AvoidContainer, LogoImage, Card, Title, InputContainer, Input, Button, ButtonText, ButtonLoginText, ButtonLogin } from './styles';
 
 import logo from '../../assets/logo.png';
+import Loader from '../../components/Loader';
+import api from '../../services/api';
 
 const ForgotPassword: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [isValidUsername, setIsvalidUsername] = useState(false);
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [temporaryPassword, setTemporaryPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
     const navigator = useNavigation();
 
     const handleGoLogin = useCallback(() => {
         navigator.navigate('Login');
-    }, [ navigator ]);
-    
+    }, [navigator]);
+
+    const handleSubmitUser = useCallback(async () => {
+        setLoading(true);
+        try {
+
+            const { status, data } = await api.post('/nova-senha', {
+                "email": "email@email.com",
+                "login": username,
+            });
+
+            if (status === 200 || status === 201) {
+                setTemporaryPassword(data);
+                setIsvalidUsername(true);
+            }
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }, [username]);
+
+    const handleSubmitNewPassword = useCallback(async () => {
+        setLoading(true);
+        console.log(password, confirmPassword);
+        try {
+            if (password !== confirmPassword) {
+                // Validation TO DO
+                console.log('err');
+                return;
+            }
+
+            const data = await api.post(`altera-senha?senhaTemporaria=${temporaryPassword}`, {
+                "senha": password,
+                "usuario": username,
+            });
+
+            if (data.status === 200 || data.status === 201) {
+                navigator.navigate('Login');
+            } else {
+                navigator.navigate('ForgetPassword');
+            }
+        } catch (err) {
+            // Error TO DO 
+        } finally {
+            setLoading(false);
+        }
+    }, [password, confirmPassword]);
+
     return (
         <ScrollContainer>
             <Container>
-                <AvoidContainer enabled={Platform.OS === 'ios'} behavior="padding" >
-
+                <AvoidContainer enabled={Platform.OS === 'ios'} behavior="padding">
                     <LogoImage source={logo} />
                     <Card>
                         <Title>Redefinir senha</Title>
-                        <InputContainer>
-                            <Input placeholder="Nome de usuário" />
-                            <Input placeholder="Nova senha" />
-                            <Input placeholder="Confirmar nova senha"/>
-                            <Button>
-                                <ButtonText>Continuar</ButtonText>
-                                <Feather name="arrow-right" size={20} color="#fff" />
-                            </Button>
-                        </InputContainer>
-                        <ButtonLogin onPress={ handleGoLogin } >
+                        {isValidUsername ? (
+                            <InputContainer>
+                                <Input secureTextEntry={true} placeholder="Nova senha" />
+                                <Input secureTextEntry={true} placeholder="Confirmar nova senha" />
+                                {loading ? (
+                                    <Loader marginTop={30} />
+                                ) : (
+                                        <Button onPress={handleSubmitNewPassword}>
+                                            <ButtonText>Continuar</ButtonText>
+                                            <Feather name="arrow-right" size={20} color="#fff" />
+                                        </Button>
+                                    )}
+                            </InputContainer>
+                        ) : (
+                                <InputContainer hasStyle>
+                                    <Input onChangeText={(text) => setUsername(text)} placeholder="Nome de usuário" />
+                                    {loading ? (
+                                        <Loader marginTop={30} />
+                                    ) : (
+                                            <Button onPress={handleSubmitUser}>
+                                                <ButtonText>Continuar</ButtonText>
+                                                <Feather name="arrow-right" size={20} color="#fff" />
+                                            </Button>
+                                        )}
+
+                                </InputContainer>
+                            )}
+
+                        <ButtonLogin onPress={handleGoLogin} >
                             <ButtonLoginText>Ir para login &gt;</ButtonLoginText>
                         </ButtonLogin>
                     </Card>
                 </AvoidContainer>
             </Container>
         </ScrollContainer>
-  );
+    );
 }
 
 export default ForgotPassword;
