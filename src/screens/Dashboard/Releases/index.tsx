8a9b15a0769a6207a08025bfc8a468d7821a83bc
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-26 22:56:55 -0300

    Preparing main dashboard application

--- src/screens/Dashboard/Releases/index.tsx
+++ src/screens/Dashboard/Releases/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Balance from '../../../components/Balance';
+import User from '../../../components/User';
+import Plans from '../../../components/Plans';
 import { Container } from './style';
+import { ScrollView } from 'react-native-gesture-handler';
+import Launchs from '../../../components/Launchs';
 
 // import { Container } from './styles';
 
 const Releases: React.FC = () => {
   return (
-    <Container>
-      <Text>
-        Lancamentos
-      </Text>
-    </Container>
+    <ScrollView>
+      <Container>
+        <User />
+        <Balance />
+        <Plans />
+        <Launchs />
+      </Container>
+    </ScrollView>
+    
   );
 }
 
