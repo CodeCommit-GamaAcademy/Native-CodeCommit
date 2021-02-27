e367bf1f0abcaba5122601a55f46b74808e75faa
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-27 18:37:21 -0300

    setting logic to take data in api.

--- src/screens/Dashboard/Releases/index.tsx
+++ src/screens/Dashboard/Releases/index.tsx
@@ -1,110 +1,81 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Balance from '../../../components/Balance';
 import User from '../../../components/User';
 import Plans from '../../../components/Plans';
 import { Container } from './style';
 import { ScrollView } from 'react-native-gesture-handler';
 import Launchs from '../../../components/Launchs';
-import { Conta, Contas } from '../../../interfaces/dashboard';
+import { Contas, Lancamentos } from '../../../interfaces/dashboard';
 import api from '../../../services/api';
-import { useNavigation } from '@react-navigation/native';
-
-// import { Container } from './styles';
+import { ApplicationStore } from '../../../store';
+import { View } from 'react-native';
 
 const Releases: React.FC = () => {
-  const navigator = useNavigation();
-  const [loading, setLoading] = useState(false);
-  const [ login, setLogin ] = useState('');
-  const [ token, setToken ] = useState('');
+  //setting store, and some states
+  const store = useSelector( (store: ApplicationStore) => store );
+  const [ allLaunchs, setAllLaunchs ] = useState<Lancamentos[]>();
+  const [ accountInfo, setAccountInfo ] = useState<Contas>();
+  const [ loading, setLoading ] = useState(false);
 
-  const loadDashInformations = useCallback(async () => {
-    setLoading(true);
+  //function to make month data get a 0 in position [0]
+  //if your length is less than 2. 
+  const formatDate = useCallback((date:string) => {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+    return [year, month, day].join('-');
+  }, []);
+
+  //function to load informations about user, like
+  //accountBank, accountCredit etc.
+  const loadDashInformations = async () => {
+    setLoading(false);
+    const today = new Date();
     const date = new Date();
-    const referenceDate = new Date(date.setDate(date.getMonth() - 120));
-    
+    const referenceDate = new Date(date.setDate(date.getMonth() - 365));
     try {
-      const response = await api.get<Contas>(`dashboard?fim=${date}&inicio=${referenceDate}&login=${login}`, {
+      const response = await api.get<Contas>(`dashboard?fim=${formatDate(today.toDateString())}&inicio=${formatDate(referenceDate.toDateString())}&login=${store.user?.login}`, {
         headers: {
-          Authorization: ''
+          Authorization: store.user?.token
         }
       });
-      // props.contaBanco?.lancamentos && props.contaCredito?.lancamentos
-      // if () {
-      //   const initLaunchs = [...props.contaBanco.lancamentos, ...props.contaCredito.lancamentos];
-
-      //   const orderedLauchs = initLaunchs.slice().sort((a, b) => {
-      //       return Number(new Date(a.data)) - Number(new Date(b.data));
-      //   }).reverse();
-
-      //   return orderedLauchs;
-      // } else {
-      //     return [];
-      // }
-
+      //make launchs ordered
+      const initLaunchs = [...response.data.contaBanco.lancamentos, 
+        ...response.data.contaCredito.lancamentos];
+      const orderedLauchs = initLaunchs.slice().sort((a, b) => {
+        return Number(new Date(a.data)) - Number(new Date(b.data));
+      }).reverse();
+      setAllLaunchs(orderedLauchs);
+      setAccountInfo(response.data);
+      setLoading(true);
     } catch (err) {
       // console.log(username, password);
-    } finally {
-      setLoading(false);
     }
-
-  }, [ token ]);
-
-  // const allLaunchs = useMemo(() => {
-  //   if (props.contaBanco?.lancamentos && props.contaCredito?.lancamentos) {
-  //       const initLaunchs = [...props.contaBanco.lancamentos, ...props.contaCredito.lancamentos];
-
-  //       const orderedLauchs = initLaunchs.slice().sort((a, b) => {
-  //           return Number(new Date(a.data)) - Number(new Date(b.data));
-  //       }).reverse();
-
-  //       return orderedLauchs;
-  //   } else {
-  //       return [];
-  //   }
-  // }, [props.contaBanco?.lancamentos, props.contaCredito?.lancamentos]);
-
-
-  const Account: Conta = {
-    id: 1,
-    lancamentos: [
-      {
-        conta: 515,
-        data: 'hoje',
-        id: 1,
-        plano: {
-          descricao: 'teste',
-          id: 1,
-          login: 'gabriel',
-          padrao: true,
-          tipoMovimento: 'R',
-        },
-        tipo: 'R',
-        valor: 10,
-      },
-      {
-        conta: 515,
-        data: 'hoje',
-        id: 1,
-        plano: {
-          descricao: 'teste',
-          id: 1,
-          login: 'gabriel',
-          padrao: true,
-          tipoMovimento: 'D',
-        },
-        tipo: 'D',
-        valor: 20,
-      }
-    ],
-    saldo: 1240.50,
-  }
+  };
+  //hook to call loadDashInformations everytime that it
+  //has rendered
+  useEffect( () => {
+    loadDashInformations();
+  }, [])
 
   return (
     <ScrollView>
       <Container>
         <User />
-        <Balance conta={Account}/>
-        <Plans lancamentos={Account.lancamentos}/>
+        {
+          loading && accountInfo ? <Balance conta={accountInfo?.contaBanco}/> : <View></View>
+        }
+        
+        {
+          loading && allLaunchs ? <Plans lancamentos={allLaunchs}/> : <View></View>
+        }
         <Launchs />
       </Container>
     </ScrollView>
