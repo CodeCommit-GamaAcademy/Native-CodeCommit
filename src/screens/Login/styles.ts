cc6a3dba444dd677c780599a872e5793b4e0a6fb
Igor Santos <igornzds@gmail.com>
2021-02-27 11:56:30 -0300

    update login page style

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -3,29 +3,27 @@ import styled from 'styled-components/native';
 
 export const LogoImg = styled.Image`
   margin-bottom: 60px;
-  margin-top: 15px
 `;
-
-export const SafeAreaContainer = styled.SafeAreaView`
-  background-color: #8c52e5;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  height: ${ Dimensions.get('window').height }px;
-`
 
 export const ContainerScrollView = styled.ScrollView`
   flex: 1;
+  background-color: #8c52e5;
+`;
+
+export const SafeAreaContainer = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  height: ${Dimensions.get('window').height}px;
+  margin: 20px 0;
 `;
 
 export const Container = styled.KeyboardAvoidingView`
-    align-items: center;
-    background-color: #8c52e5;
-    justify-content: center;
-    flex: 1;
-    height: 100%;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  height: 100%;
 `;
-
 
 export const CardForm = styled.View`
   background: #fff;
@@ -51,18 +49,17 @@ export const InputForm = styled.TextInput<{ isLastChild?: boolean }>`
   border-bottom-width: 1px;
   font-size: 14px;
   padding: 12px 6px;
-  margin-bottom: ${({ isLastChild }) => isLastChild ? 0 : 60}px;
-`
-
+  margin-bottom: ${({ isLastChild }) => (isLastChild ? 0 : 60)}px;
+`;
 
 export const ButtonSubmit = styled.TouchableOpacity`
-  background: #68DE5A;
+  background: #68de5a;
   margin-top: 50px;
   padding: 16px 20px;
   border-radius: 20px;
   flex-direction: row;
   justify-content: space-between;
-`
+`;
 
 export const ButtonSubmitText = styled.Text`
   color: #fff;
@@ -74,16 +71,14 @@ export const ButtonSubmitText = styled.Text`
 export const LinkForm = styled.TouchableOpacity<{ isLastChild?: boolean }>`
   margin-bottom: 16px;
   text-align: center;
-  margin-top: ${({ isLastChild }) => isLastChild ? 0 : 50}px;
-  margin-bottom: ${({ isLastChild }) => isLastChild ? 0 : 16}px;
+  margin-top: ${({ isLastChild }) => (isLastChild ? 0 : 50)}px;
+  margin-bottom: ${({ isLastChild }) => (isLastChild ? 0 : 16)}px;
   flex-direction: row;
   justify-content: center;
 `;
 
 export const LinkFormText = styled.Text`
   text-align: center;
-  color: #8C52E5;
+  color: #8c52e5;
   margin-right: 4px;
 `;
-
-
