aa68558be1a5e92b848388b06a253b2c739418c7
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-27 21:37:40 -0300

    setting a bottom component style

--- src/screens/Dashboard/Releases/style.ts
+++ src/screens/Dashboard/Releases/style.ts
@@ -5,3 +5,32 @@ export const Container = styled.SafeAreaView`
     align-items: center;
     background: #8C52E5;
 `;
+
+
+export const Main = styled.SafeAreaView`
+  flex: 1;
+  background: #8C52E5;
+`
+
+export const MenuLeft = styled.SafeAreaView`
+    position: absolute;
+    width: 80%;
+    height: 100%;
+    background: #FFFFFF;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 100;
+`
+
+export const MenuContainer = styled.SafeAreaView`
+    background: #000000;
+    opacity: 0.7;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+`
