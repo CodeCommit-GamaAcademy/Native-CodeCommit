2a59c3fef0b32c68af3fab01b71e03e9c6399bc1
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-28 18:49:58 -0300

    including redux store in transfer to get user data

--- src/screens/Dashboard/Transfer/index.tsx
+++ src/screens/Dashboard/Transfer/index.tsx
@@ -2,15 +2,17 @@ import { Feather } from '@expo/vector-icons';
 import React, { useState } from 'react';
 import { Platform, View } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
+import { useSelector } from 'react-redux';
 import Bottom from '../../../components/Bottom';
 
 import User from '../../../components/User';
+import { ApplicationStore } from '../../../store';
 import { ContainerScrollView, Container, InputForm, ContainerForm, ButtonSubmit, ButtonSubmitText, TitleForm, SelectForm, Main } from './style';
 
 // import { Container } from './styles';
 
 const Transfer: React.FC = () => {
-
+  const store = useSelector( (store: ApplicationStore) => store );
   const [plano, setPlano] = useState('')
   const [transacao, setTransacao] = useState('')
 
@@ -18,8 +20,7 @@ const Transfer: React.FC = () => {
     <Main>
       <ContainerScrollView>
         <Container enabled={Platform.OS === 'ios'} behavior="padding">
-          <User showCancel={true} />
-
+          { store.user && <User user={store.user} showCancel={true} />}
           <ContainerForm>
             <TitleForm>
               Transferências
