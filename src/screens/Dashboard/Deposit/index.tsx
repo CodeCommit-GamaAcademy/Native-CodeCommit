d84a731461673fc0aba758970d705b366134447f
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-27 21:37:21 -0300

    adding Main tag in components

--- src/screens/Dashboard/Deposit/index.tsx
+++ src/screens/Dashboard/Deposit/index.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { Container, ScrollContainer, TitleContainer, Title, DepositCard, HeaderCardContainer, CardTitle, InputContainer, Input, ButtonSubmit, ButtonText } from './style';
+import { Main } from '../Releases/style';
+import Bottom from '../../../components/Bottom';
 
 
 const Deposit: React.FC = () => {
   return (
-    <ScrollContainer>
-      <Container>
-        <TitleContainer>
-          <Title>Olá, Usuário</Title>
-          <Feather name="x" size={20} color="#fff" />
-        </TitleContainer>
-          <DepositCard>
-            <HeaderCardContainer>
-              <CardTitle>Depósitos</CardTitle>
-            </HeaderCardContainer>
-            <InputContainer>
-              <Input placeholder="Destinatário"></Input>
-              <Input placeholder="Plano de conta"></Input>
-              <Input placeholder="Tipo de transação"></Input>
-              <Input placeholder="Valor de depósito"></Input>
-              <ButtonSubmit>
-                <ButtonText>Realizar depósito</ButtonText>
-                <Feather name="arrow-right" size={20} color='#fff' />
-              </ButtonSubmit>
-            </InputContainer>
-          </DepositCard>
-      </Container>
-    </ScrollContainer>
+    <Main>
+      <ScrollContainer>
+        <Container>
+          <TitleContainer>
+            <Title>Olá, Usuário</Title>
+            <Feather name="x" size={20} color="#fff" />
+          </TitleContainer>
+            <DepositCard>
+              <HeaderCardContainer>
+                <CardTitle>Depósitos</CardTitle>
+              </HeaderCardContainer>
+              <InputContainer>
+                <Input placeholder="Destinatário"></Input>
+                <Input placeholder="Plano de conta"></Input>
+                <Input placeholder="Tipo de transação"></Input>
+                <Input placeholder="Valor de depósito"></Input>
+                <ButtonSubmit>
+                  <ButtonText>Realizar depósito</ButtonText>
+                  <Feather name="arrow-right" size={20} color='#fff' />
+                </ButtonSubmit>
+              </InputContainer>
+            </DepositCard>
+        </Container>
+      </ScrollContainer>
+      <Bottom />
+    </Main>
   );
 }
 
--- src/screens/Dashboard/Transfer/index.tsx
+++ src/screens/Dashboard/Transfer/index.tsx
@@ -1,24 +1,28 @@
 import { View, Text, TextInput } from 'react-native';
-import { Container, TopTitle, BoxContent , InputForm, CardTitle} from './style';
+import { Container, TopTitle, BoxContent , InputForm, CardTitle, Main} from './style';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect } from 'react';
+import Bottom from '../../../components/Bottom';
 
 // import { Container } from './styles';
 
 const Transfer: React.FC = () => {
   return (
-    <Container>
-      <TopTitle> 
-        Olá, (nome do usuário)
-      </TopTitle>
-      <BoxContent>
-<CardTitle> Transferências </CardTitle>
-        <InputForm placeholder="Destinatário"></InputForm>
-        <InputForm placeholder="teste"></InputForm>
-        <InputForm placeholder="Valor da Transferência"></InputForm>
+    <Main>
+      <Container>
+        <TopTitle> 
+          Olá, (nome do usuário)
+        </TopTitle>
+        <BoxContent>
+          <CardTitle> Transferências </CardTitle>
+          <InputForm placeholder="Destinatário"></InputForm>
+          <InputForm placeholder="teste"></InputForm>
+          <InputForm placeholder="Valor da Transferência"></InputForm>
 
-      </BoxContent>
-    </Container>
+        </BoxContent>
+      </Container>
+      <Bottom />
+    </Main>
   );
 }
 
