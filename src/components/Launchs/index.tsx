62bfddc918ccd008b1238651d65bd2aed5e26e93
NoronhaGCP <inc.691@gmail.com>
2021-03-01 18:48:12 -0300

    remove unecessary state on Launchs

--- src/components/Launchs/index.tsx
+++ src/components/Launchs/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { View } from 'react-native';
 import { Lancamentos } from '../../interfaces/dashboard';
 import { Container, Line, NullValues, Paragraph, Title, Value, ValueNegative } from './style';
@@ -7,9 +7,7 @@ interface LaunchsProps {
   launchs: Lancamentos[]
 }
 
-const Launchs: React.FC<LaunchsProps> = ( props ) => {
-
-  const [ launchs, setLaunchs ] = useState<Lancamentos[]>(props.launchs);
+const Launchs: React.FC<LaunchsProps> = ( { launchs } ) => {
 
   function currencyFormat(num: number) {
     return 'R$ ' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
