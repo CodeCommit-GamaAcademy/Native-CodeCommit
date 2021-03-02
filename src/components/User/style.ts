bf92e0ad6c1e9f5a8e935ebc1e6f0b7626aaf37d
NoronhaGCP <inc.691@gmail.com>
2021-03-02 20:55:57 -0300

    remove unecessary safeAreaView

--- src/components/User/style.ts
+++ src/components/User/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
