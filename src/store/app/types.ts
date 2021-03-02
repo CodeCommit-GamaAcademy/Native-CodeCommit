1593c71766b87648cd705a7b215a87dbba1da069
NoronhaGCP <inc.691@gmail.com>
2021-03-02 17:26:06 -0300

    remove current screen from redux store

--- src/store/app/types.ts
+++
@@ -1,7 +0,0 @@
-export enum AppActions {
-    SET_CURRENT_SCREEN = 'SET_CURRENT_SCREEN'
-}
-
-export interface AppData {
-    currentScreen: string;
-}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,9 @@
 import { createStore, Store } from 'redux';
-import { AppData } from './app/types';
 import rootReducer from './rootReducer';
 import { UserData } from './user/types';
 
 export interface ApplicationStore {
   user: UserData | null;
-  app: AppData
 }
 
 const store: Store<ApplicationStore> = createStore(rootReducer);
