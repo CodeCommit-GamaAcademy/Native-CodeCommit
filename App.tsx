423adc6d3771a949f2c927fc1686cca3051c8183
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-02 14:09:19 -0300

    including toast in app

--- App.tsx
+++ App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Routes from './src/routes';
 import store from './src/store';
+import theme from './src/styles/theme';
+import { ThemeProvider } from 'styled-components'
+import { ToastProvider } from 'react-native-styled-toast'
 
 export default function App() {
     return (
         <Provider
             store={store}
         >
-            <Routes />
+            <ThemeProvider theme={theme}>
+                <ToastProvider>
+                    <Routes />
+                </ToastProvider>
+            </ThemeProvider>
+            
         </Provider>
     );
 }
