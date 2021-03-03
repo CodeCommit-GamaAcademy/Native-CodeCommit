7ab2e090fbfb51730be3cfde80ba1c3bd500bd6d
Igor Santos <igornzds@gmail.com>
2021-03-03 04:06:42 -0300

    init validation ForgotPassword page with yup

--- src/screens/ForgotPassword/index.tsx
+++ src/screens/ForgotPassword/index.tsx
@@ -1,78 +1,115 @@
-import React, { useCallback, useState } from 'react';
-import { Image } from 'react-native';
+import React, { useCallback, useRef, useState } from 'react';
+import { Image, TextInput, View } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
 import { Platform } from 'react-native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
-import { Container, ScrollContainer, AvoidContainer, LogoImage, Card, Title, InputContainer, Input, Button, ButtonText, ButtonLoginText, ButtonLogin } from './styles';
+import { Container, ScrollContainer, AvoidContainer, LogoImage, Card, Title, InputContainer, Button, ButtonText, ButtonLoginText, ButtonLogin } from './styles';
 
 import logo from '../../assets/logo.png';
 import Loader from '../../components/Loader';
 import api from '../../services/api';
 import { useToast } from 'react-native-styled-toast'
+import Input from '../../components/Input';
 
+interface UsernameFormData {
+    username: string;
+}
+
+interface PasswordFormData {
+    password: string;
+    confirmPassword: string;
+}
 
 const ForgotPassword: React.FC = () => {
     const { toast } = useToast();
     const [username, setUsername] = useState('');
-    const [isValidUsername, setIsvalidUsername] = useState(false);
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isUsernameValid, setIsUsernameValid] = useState(false);
     const [temporaryPassword, setTemporaryPassword] = useState('');
     const [loading, setLoading] = useState(false);
 
     const navigator = useNavigation();
+    const formRefUsername = useRef<FormHandles>(null);
+    const formRefPassword = useRef<FormHandles>(null);
+    const confirmPasswordInputRef = useRef<TextInput>(null);
 
     const handleGoLogin = useCallback(() => {
         navigator.navigate('Login');
     }, [navigator]);
 
-    const handleSubmitUser = useCallback(async () => {
+    const handleSubmitUsername = useCallback(async (dataProps: { username: string }) => {
         setLoading(true);
         try {
+            formRefUsername.current?.setErrors({});
+
+            const schema = yup.object().shape({
+                username: yup.string().required('Nome de usuário obrigatório'),
+            });
+
+            await schema.validate(dataProps, {
+                abortEarly: false,
+            });
 
             const { status, data } = await api.post('/nova-senha', {
                 "email": "email@email.com",
-                "login": username,
+                "login": dataProps.username,
             });
 
             if (status === 200 || status === 201) {
                 setTemporaryPassword(data);
-                setIsvalidUsername(true);
+                setIsUsernameValid(true);
+                setUsername(dataProps.username);
+            } else {
+                throw new Error('username not found!');
             }
         } catch (err) {
-            console.log(err);
-            toast(
-                { 
-                  message: 'Usuário não encontrado!', 
-                  color: 'error', 
-                  iconColor: 'error', 
-                  accentColor: 'error', 
-                  iconName: 'x' 
-                });
+            if (err instanceof yup.ValidationError) {
+                const errors = getValidationErrors(err);
+                formRefUsername.current?.setErrors(errors);
+            }
+            toast({
+                message: 'Usuário não encontrado!',
+                color: 'error',
+                iconColor: 'error',
+                accentColor: 'error',
+                iconName: 'x'
+            });
         } finally {
             setLoading(false);
         }
-    }, [username]);
+    }, []);
 
-    const handleSubmitNewPassword = useCallback(async () => {
+    const handleSubmitNewPassword = useCallback(async (dataProps: PasswordFormData) => {
         setLoading(true);
         try {
-            if (password !== confirmPassword) {
-                // Validation TO DO
-                toast(
-                    { 
-                      message: 'As senhas não batem!', 
-                      color: 'error', 
-                      iconColor: 'error', 
-                      accentColor: 'error', 
-                      iconName: 'x' 
-                    });
+            formRefPassword.current?.setErrors({});
+
+            const schema = yup.object().shape({
+                password: yup.string().min(6, 'No mínimo 6 digitos'),
+                confirmPassword: yup.string().min(6, 'No mínimo 6 digitos')
+            });
+
+            await schema.validate(dataProps, {
+                abortEarly: false
+            });
+
+            if (dataProps.password !== dataProps.confirmPassword) {
+                toast({
+                    message: 'As senhas devem ser iguais!',
+                    color: 'error',
+                    iconColor: 'error',
+                    accentColor: 'error',
+                    iconName: 'x'
+                });
                 return;
             }
 
             const data = await api.post(`altera-senha?senhaTemporaria=${temporaryPassword}`, {
-                "senha": password,
+                "senha": dataProps.password,
                 "usuario": username,
             });
 
@@ -83,20 +120,22 @@ const ForgotPassword: React.FC = () => {
                 navigator.navigate('ForgetPassword');
             }
         } catch (err) {
-            // Error TO DO 
-            console.log(err.response);
-            toast(
-                { 
-                  message: 'Ocorreu algum erro!', 
-                  color: 'error', 
-                  iconColor: 'error', 
-                  accentColor: 'error', 
-                  iconName: 'x' 
-                });
+            if (err instanceof yup.ValidationError) {
+                const errors = getValidationErrors(err);
+                formRefPassword.current?.setErrors(errors);
+            }
+
+            toast({
+                message: 'Ocorreu algum erro!',
+                color: 'error',
+                iconColor: 'error',
+                accentColor: 'error',
+                iconName: 'x'
+            });
         } finally {
             setLoading(false);
         }
-    }, [password, confirmPassword, temporaryPassword, navigator, toast]);
+    }, [username, temporaryPassword, navigator, toast]);
 
     return (
         <ScrollContainer>
@@ -105,30 +144,68 @@ const ForgotPassword: React.FC = () => {
                     <LogoImage source={logo} />
                     <Card>
                         <Title>Redefinir senha</Title>
-                        {isValidUsername ? (
+                        {isUsernameValid ? (
                             <InputContainer>
-                                <Input onChangeText={ text => setPassword(text) } value={ password } secureTextEntry={true} placeholder="Nova senha" />
-                                <Input onChangeText={ text => setConfirmPassword(text) } value={ confirmPassword } secureTextEntry={true} placeholder="Confirmar nova senha" />
-                                {loading ? (
-                                    <Loader marginTop={30} />
-                                ) : (
-                                        <Button onPress={handleSubmitNewPassword}>
-                                            <ButtonText>Continuar</ButtonText>
-                                            <Feather name="arrow-right" size={20} color="#fff" />
-                                        </Button>
-                                    )}
+                                <View>
+                                    <Form ref={formRefPassword} onSubmit={handleSubmitNewPassword}>
+                                        <Input
+                                            autoCapitalize="none"
+                                            name="password"
+                                            placeholder="Digite sua senha"
+                                            returnKeyType="next"
+                                            secureTextEntry
+                                            onSubmitEditing={() => {
+                                                confirmPasswordInputRef.current?.focus();
+                                            }}
+                                        />
+
+                                        <Input
+                                            ref={confirmPasswordInputRef}
+                                            name="confirmPassword"
+                                            autoCapitalize="none"
+                                            placeholder="Confirmar nova senha"
+                                            secureTextEntry={true}
+                                            returnKeyType="send"
+                                            onSubmitEditing={() => {
+                                                formRefPassword.current?.submitForm();
+                                            }}
+                                        />
+                                        {loading ? (
+                                            <Loader marginTop={30} />
+                                        ) : (
+                                                <Button onPress={() => {
+                                                    formRefPassword.current?.submitForm();
+                                                }}>
+                                                    <ButtonText>Continuar</ButtonText>
+                                                    <Feather name="arrow-right" size={20} color="#fff" />
+                                                </Button>
+                                            )}
+                                    </Form>
+                                </View>
                             </InputContainer>
                         ) : (
                                 <InputContainer hasStyle>
-                                    <Input value={ username } onChangeText={(text) => setUsername(text)} placeholder="Nome de usuário" />
-                                    {loading ? (
-                                        <Loader marginTop={30} />
-                                    ) : (
-                                            <Button onPress={handleSubmitUser}>
-                                                <ButtonText>Continuar</ButtonText>
-                                                <Feather name="arrow-right" size={20} color="#fff" />
-                                            </Button>
-                                        )}
+                                    <Form ref={formRefUsername} onSubmit={handleSubmitUsername}>
+                                        <Input
+                                            name="username"
+                                            autoCapitalize="none"
+                                            placeholder="Nome de usuário"
+                                            returnKeyType="send"
+                                            onSubmitEditing={() => {
+                                                formRefUsername.current?.submitForm();
+                                            }}
+                                        />
+                                        {loading ? (
+                                            <Loader marginTop={30} />
+                                        ) : (
+                                                <Button onPress={() => {
+                                                    formRefUsername.current?.submitForm();
+                                                }}>
+                                                    <ButtonText>Continuar</ButtonText>
+                                                    <Feather name="arrow-right" size={20} color="#fff" />
+                                                </Button>
+                                            )}
+                                    </Form>
 
                                 </InputContainer>
                             )}
