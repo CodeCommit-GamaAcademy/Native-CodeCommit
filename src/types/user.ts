a24f7d0af2253da326cc713855b7e2b0ae3ef0b9
NoronhaGCP <inc.691@gmail.com>
2021-03-03 08:42:05 -0300

    Recover user types

---
+++ src/types/user.ts
@@ -0,0 +1,38 @@
+interface User {
+    cpf: string,
+    id: number,
+    login: string,
+    nome: string,
+    redefinirSenha: boolean,
+    senha: string,
+    senhaTemporaria: string
+}
+
+export interface UserResponse {
+    conta: {
+        descricao: string,
+        id: number,
+        numero: string,
+        saldo: number,
+        tipo: "CB"
+    },
+    contaCredito: {
+        descricao: string,
+        id: number,
+        numero: string,
+        saldo: number,
+        tipo: "CC"
+    },
+    dataFim: Date,
+    dataInicio: Date,
+    token: string,
+    usuario: User;
+}
+
+export interface TokenPayload {
+    sub: string;
+    idUsuario: number;
+    authorities: string[];
+    iat: number;
+    exp: number;
+}
